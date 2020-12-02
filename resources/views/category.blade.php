eaceb00e26883f4020511a356d639f38a1e47e60
@vandaijvit <vandai@jv-it.com.vn>
2020-12-02 21:12:30 +0700

    show images category

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -8,12 +8,31 @@
     <div class="div-content" id="div-content">
     @foreach ($resources as $resource)
         <form>
-            <label for="">Html:</label><br>
-            <textarea name="html" rows="4" cols="50">{{$resource->html}}</textarea><br>
-            <label for="">Sass:</label><br>
-            <textarea name="sass" rows="4" cols="50">{{$resource->sass}}</textarea><br>
-            <label for="">Task:</label><br>
-            <textarea name="task" rows="4" cols="50">{{$resource->task}}</textarea><br>
+            <div class="item-form">
+                <label for="">Html:</label><br>
+                <textarea name="html" rows="4" cols="50">{{$resource->html}}</textarea>
+            </div>
+            <div class="item-form">
+                <label for="">Sass:</label><br>
+                <textarea name="sass" rows="4" cols="50">{{$resource->sass}}</textarea>
+            </div>
+            @if($resource->images)
+            <div class="item-form">
+                <label for="">Images:</label><br>
+                <div class="image_preview">
+                    <?php 
+                        $images = explode('|', $resource->images);
+                    ?>
+                    @foreach($images as $image)
+                        <img src='{{$image}}' alt="image"><br>
+                    @endforeach
+                </div>
+            </div>
+            @endif
+            <div class="item-form">
+                <label for="">Task:</label><br>
+                <textarea name="task" rows="4" cols="50">{{$resource->task}}</textarea>
+            </div>
         </form> 
         <hr>
     @endforeach
@@ -35,12 +54,29 @@
                     }
                     for (var item of result) {
                         html += '<form>';
+                        html += '<div class="item-form">';
                         html += '<label for="">Html:</label><br>';
-                        html += '<textarea name="html" rows="4" cols="50">'+ item.html +'</textarea><br>';
+                        html += '<textarea name="html" rows="4" cols="50">'+ item.html +'</textarea>';
+                        html += '</div>'; 
+                        html += '<div class="item-form">';
                         html += '<label for="">Sass:</label><br>';
-                        html += '<textarea name="sass" rows="4" cols="50">'+ item.sass +'</textarea><br>';
+                        html += '<textarea name="sass" rows="4" cols="50">'+ item.sass +'</textarea>';
+                        html += '</div>'; 
+                        if(item.images){
+                            let images = item.images.split("|");
+                            html += '<div class="item-form">';
+                            html += '    <label for="">Images:</label><br>';
+                            html += '    <div class="image_preview">';
+                            for(var image of images) {
+                                html += '      <img src="' + image + '" alt="image">';
+                            }
+                            html += '    </div>';
+                            html += '</div>';
+                        }
+                        html += '<div class="item-form">';
                         html += '<label for="">Task:</label><br>';
-                        html += '<textarea name="task" rows="4" cols="50">'+ item.task +'</textarea><br>';
+                        html += '<textarea name="task" rows="4" cols="50">'+ item.task +'</textarea>';
+                        html += '</div>'; 
                         html += '</form>'; 
                         html += '<hr>';
                     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,31 @@
     @if(isset($resource))
     <form action="/resource" method="POST">
         @csrf
-        <label for="">Html:</label><br>
-        <textarea name="html" rows="4" cols="50">{{$resource->html}}</textarea><br>
-        <label for="">Sass:</label><br>
-        <textarea name="sass" rows="4" cols="50">{{$resource->sass}}</textarea><br>
-        <label for="">Task:</label><br>
-        <textarea name="task" rows="4" cols="50">{{$resource->task}}</textarea><br>
+        <div class="item-form">
+            <label for="">Html:</label><br>
+            <textarea name="html" rows="4" cols="50">{{$resource->html}}</textarea>
+        </div>
+        <div class="item-form">
+            <label for="">Sass:</label><br>
+            <textarea name="sass" rows="4" cols="50">{{$resource->sass}}</textarea>
+        </div>
+        @if($resource->images)
+            <div class="item-form">
+                <label for="">Images:</label><br>
+                <div class="image_preview">
+                    <?php 
+                        $images = explode('|', $resource->images);
+                    ?>
+                    @foreach($images as $image)
+                        <img src='{{$image}}' alt="image"><br>
+                    @endforeach
+                </div>
+            </div>
+        @endif
+        <div class="item-form">
+            <label for="">Task:</label><br>
+            <textarea name="task" rows="4" cols="50">{{$resource->task}}</textarea>
+        </div>
     </form>      
     @endif
 @endsection
